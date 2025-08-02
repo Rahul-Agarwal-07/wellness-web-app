e8ee5e6f3fe68d3ff7f02870da3a574988d72cd8
Rahul Agarwal <iamrpg@07gmail.com>
2025-08-02 21:06:14 +0530

    Backend Fix

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,9 @@ mongoose.connect(process.env.MONGO_URI, {
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api',sessionRoutes);
+app.get("/", (req, res) => {
+  res.status(200).send("API is running");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
